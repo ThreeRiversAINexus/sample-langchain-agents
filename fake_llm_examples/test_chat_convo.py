66252c60af0ed676c57e736d1289b0278def2121
Three Rivers AI Nexus LLC <threeriversainexus@gmail.com>
2024-02-27 19:29:23 +0000

    testing chat convo

--- fake_llm_examples/test_chat_convo.py
+++ fake_llm_examples/test_chat_convo.py
@@ -7,17 +7,15 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain_core.messages.human import HumanMessage
+from langchain_core.messages.ai import AIMessage
+import unittest
 
-test_user_messages = [
-    "hello",
-    "who are you?",
-    "how are you?"
-]
-test_responses = [
-    "The user is initiating a conversation with a greeting.",
-    "I'm just a language model AI, I'm here to help you!",
-    "I am well"
-]
+test_chats = {
+    "hello": "The user is initiating a conversation with a greeting.",
+    "who are you?": "I'm just a language model AI, I'm here to help you!",
+    "how are you?": "I am well"
+}
 # https://python.langchain.com/docs/modules/memory/
 prompt = ChatPromptTemplate(
     messages = [
@@ -28,10 +26,11 @@ prompt = ChatPromptTemplate(
         HumanMessagePromptTemplate.from_template("{user_message}")
     ]
 )
+
 memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True)
 
 # https://api.python.langchain.com/en/latest/llms/langchain_community.llms.fake.FakeListLLM.html#langchain_community.llms.fake.FakeListLLM
-fake_llm = FakeListLLM(responses=test_responses)
+fake_llm = FakeListLLM(responses=[chat for chat in test_chats.values()])
 
 conversation = LLMChain(
     llm=fake_llm,
@@ -40,9 +39,20 @@ conversation = LLMChain(
     memory=memory,
 )
 
-for message in test_user_messages:
-    response = conversation({"user_message": message})
-    print(response)
-
-# output = fake_llm.invoke(messages)
-# print(output)
+class TestChatConvo(unittest.TestCase):
+    def test_chat_convo(self):
+        ideal_chat_history = []
+        for message in test_chats.keys():
+            response = conversation.invoke({"user_message": message})
+            self.assertEqual(response["text"], test_chats[message])
+            self.assertListEqual(response["chat_history"], ideal_chat_history)
+            # We must insert these before the next iteration
+            # because the chat_history is blank for the first
+            # message
+            ideal_chat_history += [
+                HumanMessage(content=message),
+                AIMessage(content=test_chats[message])
+            ]
+    
+if __name__ == '__main__':
+    unittest.main()
